dbe34e3a3c53bc14f9770d131d20433b21b072e1
shwetacctech <149310316+shwetacctech@users.noreply.github.com>
2023-11-28 16:39:30 +0530

    Add files via upload

---
+++ Cpp advance/Ass13/Ass13/Advance_cpp/headers/Interfacing.h
@@ -0,0 +1,14 @@
+#ifndef INTERFACING_H
+#define INTERFACING_H
+
+#ifdef __cplusplus
+extern "C"{
+    #include <stdio.h>
+    void printSuccess();
+
+}
+#else
+#include <stdio.h>
+void printSuccess();
+#endif
+#endif
---
+++ Cpp advance/Ass13/Ass13/Advance_cpp/src/Interfacing.cpp
@@ -0,0 +1,5 @@
+#include "../headers/Interfacing.h"
+
+void printSuccess(){
+    printf("The operation is successful via Interfacing C");
+}
